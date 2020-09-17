199b858bcef2f529f1a913a4b108602f4a662f40
Elliott <elliottmcnary@gmail.com>
2020-09-17 10:50:45 -0700

    Fix dumb shit

--- ios/MapboxNavigationView.swift
+++ ios/MapboxNavigationView.swift
@@ -40,24 +40,48 @@ class MapboxNavigationView: UIView, NavigationViewControllerDelegate {
     
     let options = NavigationRouteOptions(waypoints: [originWaypoint, destinationWaypoint])
     
-    Directions.shared.calculate(options) { _, routes, _ in
-      guard let route = routes?.first else { return }
-      
-      let navigationService = MapboxNavigationService(route: route, simulating: self.shouldSimulateRoute ? .always : .onPoorGPS)
-      
-      self.voiceController = CustomVoiceController(navigationService: navigationService)
-      self.voiceController?.isMuted = self.isMuted
-      
-      let navigationOptions = NavigationOptions(navigationService: navigationService, voiceController: self.voiceController)
-      let navigationViewController = NavigationViewController(for: route, options: navigationOptions)
-      navigationViewController.delegate = self
-      
-      let view = navigationViewController.view!
-      view.frame = self.frame
-      view.bounds = self.bounds
-      
-      self.addSubview(view)
-    }
+    Directions.shared.calculate(options) { [weak self] (session, result) in
+        switch result {
+            case .failure(let error):
+            print(error.localizedDescription)
+            case .success(let response):
+                guard let route = response.routes?.first, let strongSelf = self else {
+                    return
+                }
+     
+                // For demonstration purposes, simulate locations if the Simulate Navigation option is on.
+                let navigationService = MapboxNavigationService(route: route, routeOptions: options, simulating: .never)
+                let navigationOptions = NavigationOptions(navigationService: navigationService)
+                let navigationViewController = NavigationViewController(for: route, routeOptions: options, navigationOptions: navigationOptions)
+//                navigationViewController.modalPresentationStyle = .fullScreen
+//                Directions.shared.calculate(options) { _, routes in
+//                  guard let route = routes?.first else { return }
+//
+//                  let navigationService = MapboxNavigationService(route: route, simulating: self.shouldSimulateRoute ? .always : .onPoorGPS)
+//
+//                  self.voiceController = CustomVoiceController(navigationService: navigationService)
+//                  self.voiceController?.isMuted = self.isMuted
+//
+//                  let navigationOptions = NavigationOptions(navigationService: navigationService, voiceController: self.voiceController)
+//                  let navigationViewController = NavigationViewController(for: route, options: navigationOptions)
+//                  navigationViewController.delegate = self
+//
+//                  let view = navigationViewController.view!
+//                  view.frame = self.frame
+//                  view.bounds = self.bounds
+//
+//                  self.addSubview(view)
+//                }
+             
+                navigationViewController.delegate = strongSelf
+        
+              let view = navigationViewController.view!
+              view.frame = strongSelf.frame
+              view.bounds = strongSelf.bounds
+
+              strongSelf.addSubview(view)
+            }
+      }
   }
   
   func navigationViewController(_ navigationViewController: NavigationViewController, didUpdate progress: RouteProgress, with location: CLLocation, rawLocation: CLLocation) {
--- react-native-mapbox-navigation.podspec
+++ react-native-mapbox-navigation.podspec
@@ -1,24 +1,28 @@
-require "json"
+# frozen_string_literal: true
 
-package = JSON.parse(File.read(File.join(__dir__, "package.json")))
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 
 Pod::Spec.new do |s|
-  s.name         = "react-native-mapbox-navigation"
-  s.version      = package["version"]
-  s.summary      = package["description"]
+  s.name         = 'react-native-mapbox-navigation'
+  s.version      = package['version']
+  s.summary      = package['description']
   s.description  = <<-DESC
                   Smart Mapbox turn-by-turn routing based on real-time traffic for React Native.
-                   DESC
-  s.homepage     = "https://github.com/emin93/react-native-mapbox-navigation
-  s.license    = { :type => "Apache-2.0", :file => "LICENSE" }
-  s.authors      = { "Emin Khateeb" => "emin@emin.ch" }
-  s.platforms    = { :ios => "9.0" }
-  s.source       = { :git => "https://github.com/emin93/react-native-mapbox-navigation.git", :tag => "#{s.version}" }
+  DESC
+  s.homepage = 'https://github.com/emin93/react-native-mapbox-navigation'
+  s.license = { type: 'Apache-2.0', file: 'LICENSE' }
+  s.authors      = { 'Emin Khateeb' => 'emin@emin.ch' }
+  s.platforms    = { ios: '10.0' }
+  s.source       = { git: 'https://github.com/emin93/react-native-mapbox-navigation.git', tag: s.version.to_s }
 
-  s.source_files = "ios/**/*.{h,m,swift}"
+  s.source_files = 'ios/**/*.{h,m,swift}'
   s.requires_arc = true
+  s.dependency 'React'
+  s.dependency 'MapboxNavigation', '~> 0.40.0'
 
-  s.dependency "React"
-  s.dependency "MapboxNavigation", "~> 0.39.0"
+  s.xcconfig = {
+    'LIBRARY_SEARCH_PATHS' => '$(TOOLCHAIN_DIR)/usr/lib/swift-$(SWIFT_VERSION)/$(PLATFORM_NAME) $(inherited)'
+  }
 end
-
